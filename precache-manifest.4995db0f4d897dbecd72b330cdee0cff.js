81637baa4502add3403eb50af49ba3384c094a20
ShunshunL <shunshunlyu@protonmail.com>
2020-05-11 18:09:46 -0700

    Updates

---
+++ precache-manifest.4995db0f4d897dbecd72b330cdee0cff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6c9b347f0a8deae84628562435bf51",
+    "url": "/react-playground-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e3e832b161e6d8924065",
+    "url": "/react-playground-monsters-rolodex/static/css/main.3f935503.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-playground-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-playground-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e832b161e6d8924065",
+    "url": "/react-playground-monsters-rolodex/static/js/main.373ec828.chunk.js"
+  },
+  {
+    "revision": "91eb90199832f51a9ce0",
+    "url": "/react-playground-monsters-rolodex/static/js/runtime-main.cedddc5f.js"
+  }
+]);
